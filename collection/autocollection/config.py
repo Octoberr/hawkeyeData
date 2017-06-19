7d23093b85e40cd29d003903dc311220fec9a60a
Allen <zengyinghao@bestwise.cc>
2017-06-19 10:51:12 +0800

    add dockerfile

--- collection/autocollection/config.py
+++ collection/autocollection/config.py
@@ -3,3 +3,11 @@
 mongo_config = {}
 mongo_config['host'] = "localhost"
 mongo_config['port'] = 27017
+
+__mongo_host = os.environ.get('MONGO_HOST')
+if __mongo_host:
+    mongo_config['host'] = __mongo_host
+
+__mongo_port = os.environ.get('MONGO_PORT')
+if __mongo_port:
+    mongo_config['port'] = __mongo_port
